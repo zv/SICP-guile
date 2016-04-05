df42948aae5bd75b722d37b76bc476ceafd33414
Zephyr Pellerin <zv@nxvr.org>
2016-04-05 14:23:29 -0700

    Counting change exercise

--- sicp.rkt
+++ sicp.rkt
@@ -26,3 +26,35 @@
         ((= y 1) 2)
         (else (Ackermann (- x 1)
                  (Ackermann x (- y 1))))))
+
+(define (count-changex cnt)
+  (define change '(1 5 10 25 50))
+  (define valid-change
+    (filter (lambda (x) (<= x cnt)) change))
+  (if (= cnt 0) null
+      (map (lambda (x) (cons x (count-changex (- cnt x))))
+           valid-change)))
+;; MY MAN!
+(define (count-number-of-chng cnt)
+  (define change '(1 5 10 25 50))
+  (define valid-change
+    (filter (lambda (x) (<= x cnt)) change))
+  (if (= cnt 0) 1
+      (map (lambda (x) (count-number-of-chng (- cnt x)))
+           valid-change)))
+
+(define (count-change amount)
+  (cc amount 5))
+
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (else (+ (cc amount (- kinds-of-coins 1))
+                 (cc (- amount (first-denomination kinds-of-coins)) kinds-of-coins)))))
+
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)))
