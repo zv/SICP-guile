221fc73282a7306b5e310e75f817cd8184aa719d
Zephyr Pellerin <zv@nxvr.org>
2016-04-17 15:00:49 -0700

    Exercise: 1.32

--- sicp.rkt
+++ sicp.rkt
@@ -202,3 +202,47 @@
       (/ (* numer1 numer2) (square denom))))
   (* 4.0 (product pi-term 1 inc factor)))
 
+#| Exercise: 1.32
+|#
+(define (accumulate combiner null-value term a next b)
+  (if (null-value a b) a
+      (combiner (term a)
+                (accumulate combiner null-value term (next a) next b))))
+
+(define (average a b) (/ (+ a b) 2))
+(define (close-enough? x y)
+  (< (abs (- x y)) 0.001))
+
+(define (search f neg-point pos-point)
+  (let [(midpoint (average neg-point pos-point))]
+    (if (close-enough? neg-point pos-point)
+        midpoint
+        (let [(test-value (f midpoint))]
+          (cond ((positive? test-value)
+                 (search f neg-point midpoint))
+                ((negative? test-value)
+                 (search f midpoint pos-point))
+                (else midpoint))))))
+
+(define (half-interval-method f a b)
+  (let ((a-value (f a))
+        (b-value (f b)))
+    (cond ((and (negative? a-value) (positive? b-value))
+           (search f a b))
+          ((and (negative? b-value) (positive? a-value))
+           (search f b a))
+          (else
+           (error "Values are not of opposite sign" a b)))))
+
+(define tolerance 0.0001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let [(next (f guess))]
+      (if (close-enough? guess next) next
+          (try next))))
+  (try first-guess))
+
+
+
