f671cd68ab04cffaf0082764d795cff67d95e62a
Zephyr Pellerin <zv@nxvr.org>
2016-04-22 16:13:22 -0700

    Exercise: 2.11

--- sicp2.rkt
+++ sicp2.rkt
@@ -60,3 +60,13 @@
                             (make-interval (/ 1.0 (upper-bound y))
                                            (/ 1.0 (lower-bound y)))))))
 
+#| Exercise: 2.11
+|#
+ (define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
