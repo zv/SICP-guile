47e293162005cb4e06febc282f9eb6f503a49a60
Zephyr Pellerin <zv@nxvr.org>
2016-05-02 16:13:23 -0700

    Exercise: 2.53

--- sicp2.rkt
+++ sicp2.rkt
@@ -539,3 +539,14 @@
 
 ;;;; Symbolic Manipulation
 
+#| Exercise: 2.53
+|#
+
+;;; (list 'a 'b 'c)                         => '(a b c)
+;;; (list (list 'george))                   => '((georger))
+;;; (cdr '((x1 x2) (y1 y2)))                => '(y1 y2)
+;;; (cadr '((x1 x2) (y1 y2)))               => '(y1 y2)
+;;; (pair? (car '(a short list)))           => 'a
+;;; (memq 'red '((red shoes) (blue socks))) => null
+;;; (memq 'red '(red shoes blue socks))     => '(shoes blue socks)
+
