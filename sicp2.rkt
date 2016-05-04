d8d96d34aefd167a1ebd85b268e82e5c4f818197
Zephyr Pellerin <zv@nxvr.org>
2016-05-04 16:13:23 -0700

    Exercise: 2.57

--- sicp2.rkt
+++ sicp2.rkt
@@ -636,3 +636,33 @@
               (expt e1 e2))]
         [else `(expt ,e1 ,e2)]))
 
+#| Exercise: 2.57
+|#
+(define (make-sum a1 a2)
+  (cond [(=number? a1 0) a2]
+        [(=number? a2 0) a1]
+        [(and (number? a1) (number? a2)
+              (+ a1 a2))]
+        [else `(+ ,a1 ,a2)]))
+
+(define (make-product m1 m2)
+  (cond [(or (=number? m1 0)
+             (=number? m2 0))
+         0]
+        [(=number? m1 1) m2]
+        [(=number? m2 1) m1]
+        [(and (number? m1) (number? m2))
+         (* m1 m2)]
+        [else (list '* m1 m2)]))
+
+(define (addend s) (cadr s))
+(define (augend s)
+  (if (null? (cdddr s))
+      (caddr s)
+      `(+ ,@(cddr s))))
+(define (multiplier s) (cadr s))
+(define (multiplicand s)
+  (if (null? (cdddr s))
+      (caddr s)
+      `(* ,@(cddr s))))
+
