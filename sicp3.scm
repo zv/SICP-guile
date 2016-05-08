885b9e5636e95b89f6409b066409ba6cd8cd5de4
Zephyr Pellerin <zv@nxvr.org>
2016-05-08 16:38:31 -0700

    Exercise 3.2

--- sicp3.scm
+++ sicp3.scm
@@ -1814,3 +1814,24 @@ make-accumulator should specify the initial value of the sum;
   (lambda (n)
     (+ x n)))
 
+#| Exercise 3.2
+In software-testing applications, it is useful to be able to count
+the number of times a given procedure is called during the course of a
+computation. Write a procedure make-monitored that takes as input a procedure,
+f, that itself takes one input. The result returned by make-monitored is a third
+procedure, say mf, that keeps track of the number of times it has been called by
+maintaining an internal counter. If the input to mf is the special symbol
+how-many-calls?, then mf returns the value of the counter. If the input is the
+special symbol reset-count, then mf resets the counter to zero. For any other
+input, mf returns the result of calling f on that input and increments the
+counter. For instance, we could make a monitored version of the sqrt procedure: |#
+(define (make-monitored f)
+  (define calls 0)
+  (lambda (n)
+    (if (eq? n 'how-many-calls?)
+        calls
+        (begin
+          (set! calls (inc calls))
+          (f n)))))
+
+
