0900867ba9087c0ff6088d914abc6e9a4b6dc46b
Zephyr Pellerin <zv@nxvr.org>
2016-05-09 16:38:31 -0700

    Exercise 3.6

--- sicp3.scm
+++ sicp3.scm
@@ -1911,3 +1911,32 @@ number less than its input.136 |#
 (define (unit-circle-pred x y)
   (<= (+ (* x x) (* y y)) 1))
 
+#| Exercise 3.6
+
+It is useful to be able to reset a random-number generator to produce a sequence
+starting from a given value. Design a new rand procedure that is called with an
+argument that is either the symbol generate or the symbol reset and behaves as
+follows: (rand 'generate) produces a new random number; ((rand 'reset)
+⟨new-value⟩) resets the internal state variable to the designated ⟨new-value⟩.
+Thus, by resetting the state, one can generate repeatable sequences. These are
+very handy to have when testing and debugging programs that use random numbers. |#
+
+;; This is what I assume he meant??
+(define (rand command)
+  (case command
+    ('generate (random 10))
+    (else (λ (new) (seed->random-state new)))))
+
+;; Utilities
+(define (count-pairs x)
+  (if (not (pair? x))
+      0
+      (+ (count-pairs (car x))
+         (count-pairs (cdr x))
+         1)))
+
+;; (define (append! x y)
+;;   (set-cdr! (last-pair x) y)
+;;   x)
+
+
