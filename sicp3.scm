eb05dd12843ee3055240df7c3f4e307bda882d2e
Zephyr Pellerin <zv@nxvr.org>
2016-05-09 16:38:31 -0700

    Exercise 3.14

--- sicp3.scm
+++ sicp3.scm
@@ -1971,3 +1971,32 @@ empty x.)
 
 ;;; Answer: An infinite loop occurs (a cycle in the linked list has been made)
 
+#| Exercise 3.14: The following procedure is quite useful, although obscure:
+|#
+
+(define (mystery x)
+  (define (loop x y)
+    (if (null? x)
+        y
+        (let ((temp (cdr x)))
+          (set-cdr! x y)
+          (loop temp x))))
+  (loop x '()))
+
+#|
+Loop uses the “temporary” variable temp to hold the old value of the cdr of x,
+since the set-cdr! on the next line destroys the cdr. Explain what mystery does
+in general. Suppose v is defined by (define v (list 'a 'b 'c 'd)). Draw the
+box-and-pointer diagram that represents the list to which v is bound. Suppose
+that we now evaluate (define w (mystery v)). Draw box-and-pointer diagrams that
+show the structures v and w after evaluating this expression. What would be
+printed as the values of v and w?
+|#
+
+#|
+Answer:
+
+Mystery reverses an array "in-place"
+|#
+
+
