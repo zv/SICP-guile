32adce9b339988613c66f2ac9e357e74330f25b0
Zephyr Pellerin <zv@nxvr.org>
2016-05-09 16:38:31 -0700

    Exercise 3.12

--- sicp3.scm
+++ sicp3.scm
@@ -1940,3 +1940,26 @@ very handy to have when testing and debugging programs that use random numbers. 
 ;;   x)
 
 
+#| Exercise 3.12: The following procedure for appending lists was introduced in 2.2.1:
+
+(define (append x y)
+  (if (null? x)
+      y
+      (cons (car x) (append (cdr x) y))))
+
+Append forms a new list by successively consing the elements of x onto y. The
+procedure append! is similar to append, but it is a mutator rather than a
+constructor. It appends the lists by splicing them together, modifying the final
+pair of x so that its cdr is now y. (It is an error to call append! with an
+empty x.)
+|#
+
+(define (last-pair x)
+  (if (null? (cdr x))
+      x
+      (last-pair (cdr x))))
+
+(define x (list 'a 'b))
+(define y (list 'c 'd))
+(define z (append x y))
+
