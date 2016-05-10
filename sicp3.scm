402390cd71757f43a1b05c7482189f0b84918f0f
Zephyr Pellerin <zv@nxvr.org>
2016-05-10 16:38:31 -0700

    Exercise 3.19

--- sicp3.scm
+++ sicp3.scm
@@ -2074,3 +2074,52 @@ such lists.
   (search xs))
 
 
+#| Exercise 3.19
+Redo exercise 3.18 using an algorithm that takes only a constant amount of
+space. (This requires a very clever idea.)
+|#
+(define* (linear-cycle-search x1
+                             #:optional (x2 (cdr x1)))
+  (cond ((or (null? (cdr x1)) (null? (cdr x2))) #f)
+        ((eq? x1 x2) #t)
+        (else (linear-cycle-search (cdr x1) (cdr (cdr x2))))))
+
+
+;; -- Section 3.3.2 - Queues -----------------------
+;; ---( Utility Functions )--------------------------
+(define (make-queue) (cons '() '()))
+
+(define (front-ptr queue) (car queue))
+
+(define (rear-ptr queue) (cdr queue))
+
+(define (set-front-ptr! queue item)
+  (set-car! queue item))
+
+(define (set-rear-ptr! queue item)
+  (set-cdr! queue item))
+
+(define (empty-queue? queue)
+
+  (null? (front-ptr queue)))
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "FRONT called with an empty queue" queue)
+      (car (front-ptr queue))))
+
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue)
+           (set-front-ptr! queue new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue)
+          (else (set-cdr! (rear-ptr queue) new-pair)
+                (set-rear-ptr! queue new-pair)
+                queue))))
+
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue)
+         (error "DELETE! called with an empty queue" queue))
+        (else
+         (set-front-ptr! queue (cdr (front-ptr queue)))
+         queue)))
