53c9f42dcc8e8f4712f01de393d790c4d2b4c6cd
Zephyr Pellerin <zv@nxvr.org>
2016-07-07 00:56:38 -0700

    Exercise 4.05

--- sicp4.scm
+++ sicp4.scm
@@ -694,3 +694,43 @@ until `(oper exp)' argument returns true or no arguments remain."
                          env
                          oper))))
 
+
+#| Exercise 4.5
+Scheme allows an additional syntax for cond clauses, (⟨test⟩ => ⟨recipient⟩). If
+⟨test⟩ evaluates to a true value, then ⟨recipient⟩ is evaluated. Its value must
+be a procedure of one argument; this procedure is then invoked on the value of
+the ⟨test⟩, and the result is returned as the value of the cond expression. For
+example
+
+     (cond ((assoc 'b '((a 1) (b 2))) => cadr)
+          (else false))
+
+returns 2.
+Modify the handling of cond so that it supports this extended syntax. |#
+
+(define (cond-is-pipe? exp)
+  (if (pair? exp) (eq? (cadr exp) '=>) #f))
+
+(define (cond-recipient exp) (caddr exp))
+
+(define (expand-clauses clauses)
+  (if (null? clauses) 'false
+      (let ([first (car clauses)]
+            [rest  (cdr clauses)])
+
+        ;; check for =>
+        (if (cond-is-pipe? first)
+            (let ([test (cond-predicate first)])
+              (make-if test
+                        (list (cond-recipient first) test)
+                       (expand-clauses rest)))
+
+            ;; otherwise a normal cond applies
+            (if (cond-else-clause? first)
+                (if (null? rest) (sequence->exp (cond-actions first))
+                    (error "ELSE clause isn't last: COND->IF" clauses))
+                (make-if (cond-predicate first)
+                         (sequence->exp (cond-actions first))
+                         (expand-clauses rest)))))))
+
+
