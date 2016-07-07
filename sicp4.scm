7d1e28c439a5310456b9e2cebb396d6240e22c11
Zephyr Pellerin <zv@nxvr.org>
2016-07-07 00:55:52 -0700

    Exercise 4.01

--- sicp4.scm
+++ sicp4.scm
@@ -539,3 +539,25 @@ to the arguments, using the underlying Lisp system"
 (define (announce-output string)
   (newline) (display string) (newline))
 
+
+#| Exercise 4.1
+Notice that we cannot tell whether the metacircular evaluator evaluates operands
+from left to right or from right to left. Its evaluation order is inherited from
+the underlying Lisp: If the arguments to cons in list-of-values are evaluated
+from left to right, then list-of-values will evaluate operands from left to
+right; and if the arguments to cons are evaluated from right to left, then
+list-of-values will evaluate operands from right to left.
+
+Write a version of list-of-values that evaluates operands from left to right
+regardless of the order of evaluation in the underlying Lisp. Also write a
+version of list-of-values that evaluates operands from right to left. |#
+
+;; There are multiple ways to approach this, Erlang generally recommends
+;; "prereversing", but this has always struck me as very dirty, so I iterate
+;; backwards. |#
+(define (rtl-list-of-values exps env)
+  (if (no-operands? exps) '()
+      (let* ((left (zeval (first-operand exps) env) )
+             (right (rtl-list-of-values (rest-operands exps) env) ))
+        (cons left right))))
+
