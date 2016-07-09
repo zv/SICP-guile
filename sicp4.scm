53b15044f65fcef2c30ac98bcb1980f4269c43d0
Zephyr Pellerin <zv@nxvr.org>
2016-07-09 02:15:42 -0700

    Exercise 4.9

--- sicp4.scm
+++ sicp4.scm
@@ -893,4 +893,28 @@ Modify let->combination of Exercise 4.6 to also support named let. |#
                             (let-exprs exp)
                             (let-body exp)))))
 
+#| Exercise 4.9
+Many languages support a variety of iteration constructs, such as `do', `for',
+`while', and `until'. In Scheme, iterative processes can be expressed in terms of
+ordinary procedure calls, so special iteration constructs provide no essential
+gain in computational power. On the other hand, such constructs are often
+convenient. Design some iteration constructs, give examples of their use, and
+show how to implement them as derived expressions. |#
+(generate-accessors
+ ([while-cond cadr]
+  [while-body caddr]))
+
+(define (make-while exp)
+  (let ([body (while-body exp)]
+        [cond (while-cond exp)])
+    (if (null? cond) 'false
+        `(let while-loop ()
+           (if ,cond
+               (begin ,body
+                      (while-loop))
+               false)))))
+
+(install-procedure `(while ,(λ (exp env) (zeval (make-while exp) env))))
+
+
 (driver-loop)
