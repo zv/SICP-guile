03602e1f5753e671207b3c7f70f4b46f4784b493
Zephyr Pellerin <zv@nxvr.org>
2016-07-11 13:14:57 -0700

    Exercise 4.13

--- sicp4.scm
+++ sicp4.scm
@@ -828,15 +828,15 @@ show how to implement them as derived expressions. |#
 (install-procedure `(while ,(λ (exp env) (zeval (make-while exp) env))))
 
 #| Exercise 4.11 & Exercise 4.12
-4.11: Instead of representing a frame as a pair of lists, we can represent a
-frame as a list of bindings, where each binding is a name-value pair. Rewrite
-the environment operations to use this alternative representation.
+  4.11: Instead of representing a frame as a pair of lists, we can represent a
+  frame as a list of bindings, where each binding is a name-value pair. Rewrite
+  the environment operations to use this alternative representation.
 
-4.12: The procedures define-variable!, set-variable-value! and
-lookup-variable-value can be expressed in terms of more abstract procedures for
-traversing the environment structure. Define abstractions that capture the
-common patterns and redefine the three procedures in terms of these
-abstractions. |#
+  4.12: The procedures define-variable!, set-variable-value! and
+  lookup-variable-value can be expressed in terms of more abstract procedures for
+  traversing the environment structure. Define abstractions that capture the
+  common patterns and redefine the three procedures in terms of these
+  abstractions. |#
 (define (primitive-procedure-objects) ; redefine to maintain alist
   (map (λ (proc) (cons 'primitive (cadr proc))) primitive-procedures))
 
@@ -845,22 +845,46 @@ abstractions. |#
 
 (define (var-process var environment fn)
   (define (var-search env)
-    (if (eq? env the-empty-environment) (error "Unbound variable" var)
+    (if (eq? env the-empty-environment) (begin
+                                          (pretty-print environment)
+                                          (error "Unbound variable" var))
         (let* ([frame (first-frame env)]
                [entry (assoc var frame)])
-          (if entry (fn entry)
+          (if entry (fn frame entry)
               (var-search (enclosing-environment env))))))
   (var-search environment))
 
 (define (lookup-variable-value var env)
-  (var-process var env cdr))
+  (var-process var env (λ (_frame entry) (cdr entry))))
 
 (define (set-variable-value! var val env)
-  (var-process var env (λ (entry) (set-cdr! entry val))))
+  (var-process var env (λ (frame entry) (set-cdr! entry val))))
 
 (define (define-variable! var val env)
   (set-car! env (assoc-set! (first-frame env) var val)))
 
+
+#| Exercise 4.13
+Scheme allows us to create new bindings for variables by means of define, but
+provides no way to get rid of bindings. Implement for the evaluator a special
+form make-unbound! that removes the binding of a given symbol from the
+environment in which the make-unbound! expression is evaluated. This problem is
+not completely specified. For example, should we remove only the binding in the
+first frame of the environment? Complete the specification and justify any
+choices you make. |#
+
+#| Spec:
+`undefine' and `unset' are functions that set the name of the binding inside the
+closest stack-frame to null. |#
+(define (undefine-variable! var env)
+  (var-process var env (λ (frame entry) (set-car! entry '()))))
+
+(define (eval-undefinition exp env)
+  (undefine-variable!
+   (definition-variable exp) env)
+  'ok)
+
+(install-procedure `(undefine ,eval-undefinition))
 
 
 (define the-global-environment (setup-environment))
