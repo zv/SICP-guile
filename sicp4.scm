a75021b17ba4aa0df0aa657f7964b270564ef432
Zephyr Pellerin <zv@nxvr.org>
2016-07-12 21:16:54 -0700

    Exercise 4.18

--- sicp4.scm
+++ sicp4.scm
@@ -975,6 +975,36 @@ definitions"
 (define (make-procedure parameters body env)
   (list 'procedure parameters (scan-out-defines body) env))
 
+
+#| Exercise 4.18
+Consider an alternative strategy for scanning out definitions that translates
+the example in the text to
+
+(lambda ⟨vars⟩
+  (let ((u '*unassigned*)
+        (v '*unassigned*))
+    (let ((a ⟨e1⟩)
+          (b ⟨e2⟩))
+      (set! u a)
+      (set! v b))
+    ⟨e3⟩))
+
+Here a and b are meant to represent new variable names, created by the
+interpreter, that do not appear in the user’s program. Consider the solve
+procedure from 3.5.4:
+
+(define (solve f y0 dt)
+  (define y (integral (delay dy) y0 dt))
+  (define dy (stream-map f y))
+  y)
+
+Will this procedure work if internal definitions are scanned out as shown in
+this exercise? What if they are scanned out as shown in the text? Explain. |#
+
+; - - - - - - Solution:
+;; This wont work because the proxy-value of `y' (a) cannot be directly
+;; referenced upon the definition of `dy'
+
 ;; Section 4.2
 
 
