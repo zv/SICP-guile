e164adfd9467cc67846ccd4fb2952e1ed409e20f
Zephyr Pellerin <zv@nxvr.org>
2016-07-19 20:01:33 -0700

    Exercise 4.26

--- sicp4.scm
+++ sicp4.scm
@@ -1444,6 +1444,40 @@ in a normal-order language? |#
 ;; of `unless' provided by SICP -- on the other hand a normal-order language will
 ;; do just fine
 
+
+#| Exercise 4.26
+Ben Bitdiddle and Alyssa P. Hacker disagree over the importance of lazy
+evaluation for implementing things such as unless. Ben points out that it’s
+possible to implement unless in applicative order as a special form. Alyssa
+counters that, if one did that, unless would be merely syntax, not a procedure
+that could be used in conjunction with higher-order procedures. Fill in the
+details on both sides of the argument. Show how to implement unless as a derived
+expression (like cond or let), and give an example of a situation where it might
+be useful to have unless available as a procedure, rather than as a special
+form. |#
+(define (zv-unless condition consequent alternative)
+  (if condition alternative
+      consequent))
+
+(generate-accessors
+ ([unless-predicate cadr]
+  [unless-alternative caddr]))
+
+(define (unless-consequent exp)
+  (if (not (null? (cdddr exp)))
+      (cadddr exp)
+      'false))
+
+(define (unless->if exp)
+  (make-if
+   (unless-predicate exp)
+   (unless-consequent exp)
+   (unless-alternative exp)))
+
+(install-procedure `(unless ,(λ (exp env) (zeval (unless->if exp) env))))
+(install-analyze-procedure `(unless ,(λ (exp env) (aeval (unless->if exp) env))))
+
+;; this functions utility is for lazy bums who cant type `not'
 ;; Various evaluator utils
 (define (current-evaluator) aeval)
 (load "4/eval-arithmetic.scm")
