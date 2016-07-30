8640899bb9d6be0e14e8ca56952131558f8d3a65
Zephyr Pellerin <zv@nxvr.org>
2016-07-30 12:31:23 -0700

    Exercise 4.35

--- sicp4.scm
+++ sicp4.scm
@@ -1276,6 +1276,28 @@ a later callsite"
           ((member (car items) (cdr items)) false)
           (else (distinct? (cdr items))))))
 
+#| Exercise 4.35
+Write a procedure an-integer-between that returns an integer between two given
+bounds. This can be used to implement a procedure that finds Pythagorean
+triples, i.e., triples of integers ( i , j , k ) between the given bounds such
+that i ≤ j and i 2 + j 2 = k 2 , as follows:
+
+  (define (a-pythagorean-triple-between low high)
+    (let ((i (an-integer-between low high)))
+      (let ((j (an-integer-between i high)))
+        (let ((k (an-integer-between j high)))
+          (require (= (+ (* i i) (* j j))
+                      (* k k)))
+          (list i j k)))))
+|#
+
+(amb/infuse
+ '(define (an-integer-between low high)
+  (require (< low high))
+  (amb low
+       (an-integer-between (+ 1 low) high))))
+
+
 (include "/home/zv/z/practice/sicp/4/eval-driver.scm")
 (define the-global-environment (setup-environment))
 (amb/execute-infuse-expressions the-global-environment)
