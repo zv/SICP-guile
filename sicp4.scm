d8f426d0289f89167ea0b8cdf5049878afe2665d
Zephyr Pellerin <zv@nxvr.org>
2016-07-31 12:31:40 -0700

    Exercise 4.36

--- sicp4.scm
+++ sicp4.scm
@@ -1298,6 +1298,30 @@ that i ≤ j and i 2 + j 2 = k 2 , as follows:
        (an-integer-between (+ 1 low) high))))
 
 
+#| Exercise 4.36
+Exercise 3.69 discussed how to generate the stream of all Pythagorean triples,
+with no upper bound on the size of the integers to be searched. Explain why
+simply replacing an-integer-between by an-integer-starting-from in the procedure
+in Exercise 4.35 is not an adequate way to generate arbitrary Pythagorean
+triples. Write a procedure that actually will accomplish this. (That is, write a
+procedure for which repeatedly typing try-again would in principle eventually
+generate all Pythagorean triples.) |#
+
+(append! primitive-procedures `((sqrt ,sqrt)))
+(amb/infuse
+ '(define (pyth-triple)
+    (define k (an-integer-starting-from 1))
+    (define j (an-integer-between 1 k))
+    (define i (an-integer-between 1 j))
+    (require (= (+ (* i i) (* j j))
+                (* k k)))
+    (list i j k)))
+
+#| Solution Explanation:
+Depth first search means that an infinite `amb' loop will never progress past
+the first 'amb' |#
+
+
 (include "/home/zv/z/practice/sicp/4/eval-driver.scm")
 (define the-global-environment (setup-environment))
 (amb/execute-infuse-expressions the-global-environment)
