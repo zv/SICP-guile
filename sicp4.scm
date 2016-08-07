659447c4cd695d4f5b8f587b9aa60d648cafa5eb
Zephyr Pellerin <zv@nxvr.org>
2016-08-07 12:32:08 -0700

    Exercise 4.37

--- sicp4.scm
+++ sicp4.scm
@@ -1322,6 +1322,25 @@ Depth first search means that an infinite `amb' loop will never progress past
 the first 'amb' |#
 
 
+#| Exercise 4.37
+Ben Bitdiddle claims that the following method for generating Pythagorean
+triples is more efficient than the one in Exercise 4.35. Is he correct? (Hint:
+Consider the number of possibilities that must be explored.)
+
+    (define (a-pythagorean-triple-between low high)
+      (let ((i (an-integer-between low high))
+            (hsq (* high high)))
+        (let ((j (an-integer-between i high)))
+          (let ((ksq (+ (* i i) (* j j))))
+            (require (>= hsq ksq))
+            (let ((k (sqrt ksq)))
+              (require (integer? k))
+              (list i j k))))))
+|#
+
+;; It seems to be -- Ben's method only uses N^2 space, while the .35 method uses
+;; N^3 space, while also throwing away tons of 'impossible' results.
+
 (include "/home/zv/z/practice/sicp/4/eval-driver.scm")
 (define the-global-environment (setup-environment))
 (amb/execute-infuse-expressions the-global-environment)
