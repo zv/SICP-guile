57e8c870ec88ddf1ff9798eef5c4140b274b66b7
Zephyr Pellerin <zv@nxvr.org>
2016-08-16 21:38:59 -0700

    Exercise 4.45

--- sicp4.scm
+++ sicp4.scm
@@ -1660,6 +1660,87 @@ words: |#
               (maybe-extend (parse-simple-noun-phrase)))))
 
 
+#| Exercise 4.45
+With the grammar given above, the following sentence can be parsed in five
+different ways: "the professor lectures to the student in the class with the
+cat." Give the five parses and explain the differences in shades of meaning
+among them. |#
+
+#|
+(parse '(the professor lectures to the student in the class with the cat)) ~>
+
+(sentence
+   (simple-noun-phrase (article the) (noun professor))
+   (verb-phrase
+    (verb-phrase
+     (verb-phrase
+      (verb lectures)
+      (prep-phrase
+       (prep to)
+       (simple-noun-phrase (article the) (noun student))))
+     (prep-phrase (prep in) (simple-noun-phrase (article the) (noun class))))
+    (prep-phrase (prep with) (simple-noun-phrase (article the) (noun cat)))))
+
+
+  (sentence
+   (simple-noun-phrase (article the) (noun professor))
+   (verb-phrase
+    (verb-phrase
+     (verb lectures)
+     (prep-phrase (prep to) (simple-noun-phrase (article the) (noun student))))
+    (prep-phrase
+     (prep in)
+     (noun-phrase
+      (simple-noun-phrase (article the) (noun class))
+      (prep-phrase
+       (prep with)
+       (simple-noun-phrase (article the) (noun cat)))))))
+
+  (sentence
+   (simple-noun-phrase (article the) (noun professor))
+   (verb-phrase
+    (verb-phrase
+     (verb lectures)
+     (prep-phrase
+      (prep to)
+      (noun-phrase
+       (simple-noun-phrase (article the) (noun student))
+       (prep-phrase
+        (prep in)
+        (simple-noun-phrase (article the) (noun class))))))
+    (prep-phrase (prep with) (simple-noun-phrase (article the) (noun cat)))))
+
+  (sentence
+   (simple-noun-phrase (article the) (noun professor))
+   (verb-phrase
+    (verb lectures)
+    (prep-phrase
+     (prep to)
+     (noun-phrase
+      (noun-phrase
+       (simple-noun-phrase (article the) (noun student))
+       (prep-phrase (prep in) (simple-noun-phrase (article the) (noun class))))
+      (prep-phrase
+       (prep with)
+       (simple-noun-phrase (article the) (noun cat)))))))
+
+  (sentence
+   (simple-noun-phrase (article the) (noun professor))
+   (verb-phrase
+    (verb lectures)
+    (prep-phrase
+     (prep to)
+     (noun-phrase
+      (simple-noun-phrase (article the) (noun student))
+      (prep-phrase
+       (prep in)
+       (noun-phrase
+        (simple-noun-phrase (article the) (noun class))
+        (prep-phrase
+         (prep with)
+         (simple-noun-phrase (article the) (noun cat))))))))) |#
+
+
 (include "/home/zv/z/practice/sicp/4/eval-driver.scm")
 (define the-global-environment (setup-environment))
 (amb/execute-infuse-expressions the-global-environment)
