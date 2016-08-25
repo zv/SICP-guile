4cae23483585cfa1d139b1c0be275b30d78e3c34
Zephyr Pellerin <zv@nxvr.org>
2016-08-24 19:20:56 -0700

    Exercise 4.56

--- sicp4.scm
+++ sicp4.scm
@@ -2049,6 +2049,32 @@ Respectively:
 |#
 
 
+#| Exercise 4.56
+Formulate compound queries that retrieve the following information:
+
+a. the names of all people who are supervised by Ben Bitdiddle,
+together with their addresses;
+
+b. all people whose salary is less than Ben Bitdiddle's,
+together with their salary and Ben Bitdiddle's salary;
+
+c. all people who are supervised by someone who is not in the
+computer division, together with the supervisor's name and
+job. |#
+
+#| Solutions:
+a. (and (supervisor ?name (Bitdiddle Ben))
+        (address ?name . ?rest))
+
+b. (and (salary ?person ?salary)
+        (salary (Bitdiddle Ben) ?ben-salary)
+        (lisp-value > ?salary ?ben-salary))
+
+c. (and (supervisor ?supervisor ?supervisee)
+        (job ?supervisor ?job))
+|#
+
+
 
 
 (include "/home/zv/z/practice/sicp/evaluator/eval-driver.scm")
