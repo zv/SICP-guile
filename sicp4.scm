9adb2a5baa660c6639d1a5be632223167602872b
Zephyr Pellerin <zv@nxvr.org>
2016-08-24 19:20:53 -0700

    Exercise 4.55

--- sicp4.scm
+++ sicp4.scm
@@ -2032,6 +2032,23 @@ grab results"
 
 (install-driver-loop 'qeval query-driver-loop)
 
+#| Exercise 4.55
+Give simple queries that retrieve the following information from the data
+base:
+
+- all people supervised by Ben Bitdiddle;
+- the names and jobs of all people in the accounting division;
+- the names and addresses of all people who live in Slumerville. |#
+
+#| Solution:
+Respectively:
+
+- (supervisor ?x (Bitdiddle Ben))
+- (job ?name (accounting . ?other))
+- (address ?name (Slumerville . ?rest))
+|#
+
+
 
 
 (include "/home/zv/z/practice/sicp/evaluator/eval-driver.scm")
