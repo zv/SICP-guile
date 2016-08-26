22f839854c159f6a92ab423937ad93019cbae78a
Zephyr Pellerin <zv@nxvr.org>
2016-08-25 19:21:19 -0700

    Exercise 4.60

--- sicp4.scm
+++ sicp4.scm
@@ -2172,6 +2172,44 @@ should she make to find this out?
 ;; c. (meeting-time (Hacker Alyssa P) (Wednesday . ?x))
 
 
+#| Exercise 4.60
+By giving the query
+
+    (lives-near ?person (Hacker Alyssa P))
+
+Alyssa P. Hacker is able to find people who live near her, with
+whom she can ride to work.  On the other hand, when she tries to
+find all pairs of people who live near each other by querying
+
+    (lives-near ?person-1 ?person-2)
+
+she notices that each pair of people who live near each other is
+listed twice; for example,
+
+    (lives-near (Hacker Alyssa P) (Fect Cy D))
+    (lives-near (Fect Cy D) (Hacker Alyssa P))
+
+Why does this happen?  Is there a way to find a list of people who
+live near each other, in which each pair appears only once?
+Explain. |#
+
+#| Solution:
+The reason this occurs it that there is no 'ordering' on the results
+returned, e.g (Dewitt) (Louis) is equally valid to (Louis) (Dewitt) when
+the query analyzer searches through valid responses.
+|#
+
+(query/infuse
+ '(rule (lives-near-only ?person-1 ?person-2)
+        (and
+         (address ?person-1 (?town . ?rest-1))
+         (address ?person-2 (?town . ?rest-2))
+         (not (same ?person-1 ?person-2))
+         (lisp-value (lambda (x y)
+                       (string<=? (symbol->string (car x))
+                                  (symbol->string (car y)))) ?person-1 ?person-2))))
+
+
 
 
 (include "/home/zv/z/practice/sicp/evaluator/eval-driver.scm")
