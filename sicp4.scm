a8542fc7b0ff878af48a176e94f58b42b2785253
Zephyr Pellerin <zv@nxvr.org>
2016-08-25 19:21:24 -0700

    Exercise 4.63

--- sicp4.scm
+++ sicp4.scm
@@ -2252,6 +2252,44 @@ such as `(last-pair ?x (3))' ? |#
              (last-pair ?t ?last))))
 
 
+#| Exercise 4.63
+The following data base (see Genesis 4) traces the genealogy of the
+descendants of Ada back to Adam, by way of Cain:
+|#
+
+(map query/infuse
+     '((son Adam Cain)
+       (son Cain Enoch)
+       (son Enoch Irad)
+       (son Irad Mehujael)
+       (son Mehujael Methushael)
+       (son Methushael Lamech)
+       (wife Lamech Ada)
+       (son Ada Jabal)
+       (son Ada Jubal)))
+
+#|
+Formulate rules such as
+
+  "If S is the son of f, and f is the son of
+   G, then S is the grandson of G" and "If W is the wife of M, and S
+   is the son of W, then S is the son of M"
+
+(which was supposedly more true in biblical times than today) that will
+enable the query system to find the grandson of Cain; the sons of Lamech;
+the grandsons of Methushael. (See *Note Exercise 4-69 for some rules to
+deduce more complicated relationships.)
+|#
+
+(query/infuse
+ '(rule (grandparent ?g ?f ?s)
+      (and (son ?g ?f) (son ?f ?s))))
+
+(query/infuse
+ '(rule (son-of ?w ?m ?s)
+        (and (wife ?m ?w) (son ?w ?s))))
+
+
 
 
 (include "/home/zv/z/practice/sicp/evaluator/eval-driver.scm")
