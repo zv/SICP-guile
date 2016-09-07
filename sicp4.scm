0254b277d65938c0122d952415d064f9f4fa5292
Zephyr Pellerin <zv@nxvr.org>
2016-09-07 15:49:47 -0700

    Exercise 4.73

--- sicp4.scm
+++ sicp4.scm
@@ -2490,6 +2490,25 @@ that every element in an infinite list can be reached, rather than focusing
 on either an input stream or a database |#
 
 
+#| Exercise 4.73
+Why does flatten-stream use delay explicitly? What would be wrong with
+defining it as follows:
+
+  (define (flatten-stream stream)
+    (if (stream-null? stream)
+        the-empty-stream
+        (interleave (stream-car stream)
+                    (flatten-stream
+                    (stream-cdr stream)))))
+|#
+
+#| Solution:
+Because it would loop forever - the evaluation strategy of a scheme
+interpreter dictates that arguments are passed in their 'fully evaluated'
+form.
+|#
+
+
 
 (include "/home/zv/z/practice/sicp/evaluator/eval-driver.scm")
 (define the-global-environment (setup-environment))
