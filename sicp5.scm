68e782e3b24bfe1e46a73901a3526e0452ee4809
Zephyr Pellerin <zv@nxvr.org>
2016-11-03 21:46:10 -0700

    Exercise 5.5

--- sicp5.scm
+++ sicp5.scm
@@ -196,6 +196,11 @@ Iterative exponentiation:
                done))
 
 
+#| Exercise 5.5 DONE
+Hand-simulate the factorial and Fibonacci machines, using some nontrivial
+input (requiring execution of at least one recursive call). Show the
+contents of the stack at each significant point in the execution. |#
+
 
 (if inside-repl? 'ready ;; we want the repl available ASAP if were inside emacs
     (begin
