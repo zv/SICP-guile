7cbdaa9ece8f358f3e5a6adc2b570b7e28966072
Zephyr Pellerin <zv@nxvr.org>
2016-11-09 13:38:32 -0800

    [gui] Shortest ansi-color-table

--- machine/gui.scm
+++ machine/gui.scm
@@ -77,35 +77,21 @@
 
 ;; initialize machine
 (map (λ (elt) (set-register-contents! *machine* (car elt) (cdr elt))) '((n . 10)))
-
 (*machine* 'init)
+
 
+                                                  ; Termcap
 (define *ansi-color-tables*
-  (alist->hash-table '((CLEAR . "0")
-                       (RESET . "0")
-                       (BOLD . "1")
-                       (DARK . "2")
-                       (UNDERLINE . "4")
-                       (UNDERSCORE . "4")
-                       (BLINK . "5")
-                       (REVERSE . "6")
-                       (CONCEALED . "8")
-                       (BLACK . "30")
-                       (RED . "31")
-                       (GREEN . "32")
-                       (YELLOW . "33")
-                       (BLUE . "34")
-                       (MAGENTA . "35")
-                       (CYAN . "36")
-                       (WHITE . "37")
-                       (ON-BLACK . "40")
-                       (ON-RED . "41")
-                       (ON-GREEN . "42")
-                       (ON-YELLOW . "43")
-                       (ON-BLUE . "44")
-                       (ON-MAGENTA . "45")
-                       (ON-CYAN . "46")
-                       (ON-WHITE . "47"))))
+  (alist->hash-table
+   '((CLEAR . "0") (RESET . "0") (BOLD . "1")
+     (DARK . "2") (UNDERLINE . "4") (UNDERSCORE . "4")
+     (BLINK . "5") (REVERSE . "6") (CONCEALED . "8")
+     (BLACK . "30") (RED . "31") (GREEN . "32")
+     (YELLOW . "33") (BLUE . "34") (MAGENTA . "35")
+     (CYAN . "36") (WHITE . "37") (ON-BLACK . "40")
+     (ON-RED . "41") (ON-GREEN . "42") (ON-YELLOW . "43")
+     (ON-BLUE . "44") (ON-MAGENTA . "45") (ON-CYAN . "46")
+     (ON-WHITE . "47"))))
 
 (define (color . lst)
   (let ((color-list
