f417c01931687f71c40404d93ecff25b8d0e7d8f
Zephyr Pellerin <zv@nxvr.org>
2017-03-28 16:54:25 -0700

    Exercise 3.24

--- sicp3.scm
+++ sicp3.scm
@@ -571,6 +571,52 @@ a better structure:
 (define get (operation-table 'lookup-proc))
 (define put (operation-table 'insert-proc!))
 
+
+#| Exercise 3.24:
+In the table implementations above, the keys are tested for equality using
+`equal?' (called by `assoc'). This is not always the appropriate test. For
+instance, we might have a table with numeric keys in which we don't need an
+exact match to the number we're looking up, but only a number within some
+tolerance of it. Design a table constructor `make-table' that takes as an
+argument a `same-key?' procedure that will be used to test "equality" of
+keys. `Make-table' should return a `dispatch' procedure that can be used to
+access appropriate `lookup' and `insert!' procedures for a local table.
+|#
+
+(define (make-table same-key?)
+  (let ((local-table (list '*table*)))
+    ;; just redefine `assoc' with `same-key?'
+    (define (assoc key records)
+      (cond ((null? records) #f)
+            ((same-key? key (caar records)) (car records))
+            (else (assoc key (cdr records)))))
+    (define (lookup key-1 key-2)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (cdr record)
+                  #f))
+            #f)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (set-cdr! record value)
+                  (set-cdr! subtable
+                            (cons (cons key-2 value)
+                                  (cdr subtable)))))
+            (set-cdr! local-table
+                      (cons (list key-1
+                                  (cons key-2 value))
+                            (cdr local-table))))))
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)))
+    dispatch))
+
+
 
 
 ;; -- 3.3.4 | Constraint Satisfaction ----------------
