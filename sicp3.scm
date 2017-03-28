6fe0b1c437c12e34d4e18ed9fbe5f455431f6c59
Zephyr Pellerin <zv@nxvr.org>
2017-03-28 16:54:44 -0700

    Exercise 3.26

--- sicp3.scm
+++ sicp3.scm
@@ -632,6 +632,26 @@ directly (which `equal?' can compare)
 |#
 
 
+#|  Exercise 3.26
+To search a table as implemented above, one needs to scan through the list
+of records. This is basically the unordered list representation of section
+*Note 2-3-3. For large tables, it may be more efficient to structure the
+table in a different manner. Describe a table implementation where the
+(key, value) records are organized using a binary tree, assuming that keys
+can be ordered in some way (e.g., numerically or alphabetically). (Compare
+Exercise 2-66 of Chapter 2)
+|#
+
+#| Answer:
+The value that is to be inserted is converted into it's numeric form. Insert
+& Lookup function as you would expect
+|#
+
+#| Notes:
+This could be expanded out to actually implement the tree.
+|#
+
+
 
 
 ;; -- 3.3.4 | Constraint Satisfaction ----------------
