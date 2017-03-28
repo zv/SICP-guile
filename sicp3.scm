aa845ef77ce7bdf6e7e490a2f7de634934cac3f4
Zephyr Pellerin <zv@nxvr.org>
2017-03-28 16:54:59 -0700

    Exercise 3.27

--- sicp3.scm
+++ sicp3.scm
@@ -652,6 +652,57 @@ This could be expanded out to actually implement the tree.
 |#
 
 
+#| Exercise 3.27
+"Memoization" (also called "tabulation") is a technique that enables a
+procedure to record, in a local table, values that have previously been
+computed. This technique can make a vast difference in the performance of a
+program. A memoized procedure maintains a table in which values of previous
+calls are stored using as keys the arguments that produced the values. When
+the memoized procedure is asked to compute a value, it first checks the
+table to see if the value is already there and, if so, just returns that
+value. Otherwise, it computes the new value in the ordinary way and stores
+this in the table. As an example of memoization, recall from section
+1-2-2 the exponential process for computing Fibonacci numbers:
+
+    (define (fib n)
+      (cond ((= n 0) 0)
+            ((= n 1) 1)
+            (else (+ (fib (- n 1))
+                     (fib (- n 2))))))
+
+The memoized version of the same procedure is
+
+    (define memo-fib
+      (memoize (lambda (n)
+                 (cond ((= n 0) 0)
+                       ((= n 1) 1)
+                       (else (+ (memo-fib (- n 1))
+                                (memo-fib (- n 2))))))))
+
+where the memoizer is defined as
+
+    (define (memoize f)
+      (let ((table (make-table)))
+        (lambda (x)
+          (let ((previously-computed-result (lookup x table)))
+            (or previously-computed-result
+                (let ((result (f x)))
+                  (insert! x result table)
+                  result))))))
+
+Draw an environment diagram to analyze the computation of `(memo-fib
+3)'. Explain why `memo-fib' computes the nth Fibonacci number in a number
+of steps proportional to n. Would the scheme still work if we had simply
+defined `memo-fib' to be `(memoize fib)'?
+|#
+
+#| Answer:
+memo-fib is O(N) because the fibonacci sequence can simply be computed in
+2*(Σ(N)) steps (half of which are 'precomputed')
+
+The scheme would not work if each function were freshly memoized because
+the `table' would not be shared between the various applications of
+`memo-fib'. |#
 
 
 ;; -- 3.3.4 | Constraint Satisfaction ----------------
