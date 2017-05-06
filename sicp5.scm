b7ad3c7b49f66f5c6d8129e6926fd765e92e3963
Zephyr Pellerin <zv@nxvr.org>
2017-05-05 20:35:41 -0700

    Exercise 5.14

--- sicp5.scm
+++ sicp5.scm
@@ -334,6 +334,28 @@ I modified `make-machine' directly to support this change in rev:f68d783
 |#
 
 
+#| Exercise 5.14
+Measure the number of pushes and the maximum stack depth required to
+compute n! for various small values of n using the factorial machine shown
+Figure 5-11. From your data determine formulas in terms of n for
+the total number of push operations and the maximum stack depth used in
+computing n! for any n > 1. Note that each of these is a linear function of
+n and is thus determined by two constants. In order to get the statistics
+printed, you will have to augment the factorial machine with instructions
+to initialize the stack and print the statistics. You may want to also
+modify the machine so that it repeatedly reads a value for n, computes the
+factorial, and prints the result (as we did for the GCD machine in Figure
+5.4, so that you will not have to repeatedly invoke `get-register-contents',
+`set-register-contents!', and `start'. |#
+
+
+#| Answer:
+Because no operations pop elements off the stack until the function
+terminates, the total amount of stack space used is 2N - 2 where N is the
+space used by a particular stack frame.
+|#
+
+
 (if inside-repl? 'ready ;; we want the repl available ASAP if were inside emacs
     (begin
       ;; load our tests
