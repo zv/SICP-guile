c6a165df93b45caee846e149177a04b76fc313e2
Zephyr Pellerin <zv@nxvr.org>
2017-05-05 20:42:29 -0700

    Exercise 5.17

--- sicp5.scm
+++ sicp5.scm
@@ -377,6 +377,17 @@ messages to turn tracing on and off. |#
 #| Answer: Added to machine/gui #|
 
 
+#| Exercise 5.17
+Extend the instruction tracing of *Note Exercise 5-16 so that before
+printing an instruction, the simulator prints any labels that immediately
+precede that instruction in the controller sequence. Be careful to do this
+in a way that does not interfere with instruction counting (*Note Exercise
+5-15). You will have to make the simulator retain the necessary label
+information. |#
+
+#| Answer:
+This is already a feature :)
+|#
 
 
 (if inside-repl? 'ready ;; we want the repl available ASAP if were inside emacs
