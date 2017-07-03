9d644954cdcee1378b42181dfad6008a23af9c65
Zephyr Pellerin <zv@nxvr.org>
2017-07-03 15:42:26 -0700

    Updated Exercise 5.18

--- sicp5.scm
+++ sicp5.scm
@@ -390,7 +390,7 @@ This is already a feature :)
 |#
 
 
-#| TODO Exercise 5.18
+#| Exercise 5.18
 Modify the `make-register' procedure of section *Note 5-2-1 so that
 registers can be traced. Registers should accept messages that turn tracing
 on and off. When a register is traced, assigning a value to the register
@@ -398,6 +398,8 @@ should print the name of the register, the old contents of the register,
 and the new contents being assigned. Extend the interface to the machine
 model to permit you to turn tracing on and off for designated machine
 registers. |#
+
+#| Answer: Added in `before-set-hook'|#
 
 
 #| TODO Exercise 5.19
