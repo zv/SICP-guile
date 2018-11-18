2a92e12e380e8e9c8907ecb8762c167484ea2d52
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2018-11-18 15:00:02 -0800

    [guile] Exercise 1.3

--- sicp1.scm
+++ sicp1.scm
@@ -70,3 +70,15 @@ Translate the following expression into prefix form.
 (/ (+ (+ 5 4) (- 2 (- 3 (+ 6 (/ 4 5))))) (* 3 (- 6 2) (- 2 7)))
 |#
 
+
+#| Exercise 1.3
+Define a procedure that takes three numbers as arguments and returns the sum of
+the squares of the two larger numbers.
+|#
+
+(define (square x) (* x x))
+(define (largest-squares x y z)
+  (+
+   (if (> x y) (square x) (square y))
+   (if (> y z) (square y) (square z))))
+
