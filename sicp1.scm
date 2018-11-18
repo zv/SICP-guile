41222d229d260f9aac826c8a118c114e53a29af2
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2018-11-18 15:00:51 -0800

    [guile] Exercise 1.7

--- sicp1.scm
+++ sicp1.scm
@@ -167,3 +167,26 @@ Any function supplied to `new-if' will be applied, `sqrt-iter' will thus
 infinitely loop.
 |#
 
+
+#| Exercise 1.7
+The `good-enough?' test used in computing square roots will not be very
+effective for finding the square roots of very small numbers. Also, in real
+computers, arithmetic operations are almost always performed with limited
+precision. This makes our test inadequate for very large numbers. Explain these
+statements, with examples showing how the test fails for small and large
+numbers.
+
+An alternative strategy for implementing `good-enough?' is to watch how
+`guess' changes from one iteration to the next and to stop when the change
+is a very small fraction of the guess. Design a square-root procedure that
+uses this kind of end test. Does this work better for small and large
+numbers?
+
+|#
+
+(define (fix/sqrt-iter guess last-guess x)
+  (let ([good-enough? (< (abs (- guess last-guess)) 0.001)]
+        [next-guess (average guess (/ x guess))])
+    (if good-enough? guess
+        (fix/sqrt-iter next-guess guess x))))
+
