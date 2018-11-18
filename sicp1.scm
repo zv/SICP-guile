7c5a008687519d20261123ae5715c888f17c8e44
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2018-11-18 15:00:27 -0800

    [guile] Exercise 1.5

--- sicp1.scm
+++ sicp1.scm
@@ -97,3 +97,36 @@ If b is greater than 0, add a to b.
 Otherwise, subtract b from a
 |#
 
+
+#| Exercise 1.5
+Ben Bitdiddle has invented a test to determine whether the interpreter he is
+faced with is using applicative-order evaluation or normal-order evaluation. He
+defines the following two procedures:
+
+          (define (p) (p))
+
+          (define (test x y)
+            (if (= x 0)
+                0
+                y))
+
+Then he evaluates the expression
+
+          (test 0 (p))
+
+What behavior will Ben observe with an interpreter that uses applicative-order
+evaluation? What behavior will he observe with an interpreter that uses
+normal-order evaluation? Explain your answer. (Assume that the evaluation rule
+for the special form `if' is the same whether the interpreter is using normal or
+applicative order: The predicate expression is evaluated first, and the result
+determines whether to evaluate the consequent or the alternative expression.)
+|#
+
+#| Answer:
+An applicative order evaluator would never terminate. The value of `p' is
+expanded prior to the logic of `test' being applied.
+
+Conversely, a normal-order evaluator would return 0, it never had the chance to
+expand `p'
+|#
+
