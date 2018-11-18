48928737cbc0690d350a2fed93d6e0f21bc7fcca
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2018-11-18 15:00:41 -0800

    [guile] Exercise 1.6

--- sicp1.scm
+++ sicp1.scm
@@ -130,3 +130,40 @@ Conversely, a normal-order evaluator would return 0, it never had the chance to
 expand `p'
 |#
 
+
+#| Exercise 1.6
+Alyssa P. Hacker doesn't see why `if' needs to be provided as a special form.
+"Why can't I just define it as an ordinary procedure in terms of `cond'?" she
+asks. Alyssa's friend Eva Lu Ator claims this can indeed be done, and she
+defines a new version of `if':
+
+         (define (new-if predicate then-clause else-clause)
+           (cond (predicate then-clause)
+                 (else else-clause)))
+
+Eva demonstrates the program for Alyssa:
+
+        (new-if (= 2 3) 0 5)
+        5
+
+        (new-if (= 1 1) 0 5)
+        0
+
+Delighted, Alyssa uses `new-if' to rewrite the square-root program:
+
+        (define (sqrt-iter guess x)
+          (new-if (good-enough? guess x)
+                  guess
+                  (sqrt-iter (improve guess x)
+                            x)))
+
+What happens when Alyssa attempts to use this to compute square
+roots?  Explain.
+
+|#
+
+#| Answer:
+Any function supplied to `new-if' will be applied, `sqrt-iter' will thus
+infinitely loop.
+|#
+
