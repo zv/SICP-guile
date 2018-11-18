0e84b48eda64411b40757332f2889b77600d734b
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2018-11-18 15:02:13 -0800

    [guile] Exercise 1.13

--- sicp1.scm
+++ sicp1.scm
@@ -433,3 +433,10 @@ computes elements of Pascal's triangle by means of a recursive process.
 
   (build 0))
 
+
+#| Exercise 1.13
+Prove that Fib(n) is the closest integer to φⁿ/√5, where φ = (1 + √5)/2.
+Hint: Let ψ = (1−√5)/2. Use induction and the definition of the Fibonacci
+numbers (see section *Note 1.2.2) to prove that Fib(n) = (φⁿ - ψⁿ) / √5
+|#
+
