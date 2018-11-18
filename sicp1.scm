f22b6d958c45c1f3cea324cc5ab78349d7bf55ca
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2018-11-18 14:59:32 -0800

    [guile] Exercise 1.2

--- sicp1.scm
+++ sicp1.scm
@@ -57,4 +57,16 @@ scheme@(guile-user)> 6
 scheme@(guile-user)> 16
 |#
 
+
+#| Exercise 1.2
+Translate the following expression into prefix form.
+
+            5 + 4 + (2 - (3 - (6 + 4/5)))
+            -----------------------------
+                  3(6 - 2)(2 - 7)
+|#
+
+#| Answer:
+(/ (+ (+ 5 4) (- 2 (- 3 (+ 6 (/ 4 5))))) (* 3 (- 6 2) (- 2 7)))
+|#
 
