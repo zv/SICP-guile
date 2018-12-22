7b564f57428f22e37c52c400f993952d4724173f
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2018-12-22 15:22:34 -0800

    Exercise 1.17

--- sicp1.scm
+++ sicp1.scm
@@ -694,3 +694,30 @@ of iterative algorithms.)
    [(even? n) (zv/expt-iter (* b b) (/ n 2) a)]
    [else      (zv/expt-iter  b (- n 1) (* a b))]))
 
+
+#| Exercise 1.17
+The exponentiation algorithms in this section are based on performing
+exponentiation by means of repeated multiplication. In a similar way, one
+can perform integer multiplication by means of repeated addition. The
+following multiplication procedure (in which it is assumed that our
+language can only add, not multiply) is analogous to the `expt' procedure:
+
+          (define (* a b)
+            (if (= b 0)
+              0
+              (+ a (* a (- b 1)))))
+
+This algorithm takes a number of steps that is linear in `b'. Now suppose
+we include, together with addition, operations `double', which doubles an
+integer, and `halve', which divides an (even) integer by 2. Using these,
+design a multiplication procedure analogous to `fast-expt' that uses a
+logarithmic number of steps.
+|#
+
+(define (1.17/fast-* a b)
+  (define (double x) (+ x x))
+  (define (halve x) (/ x 2))
+  (cond ((= b 0) 0)
+        ((even? b) (double (* a (halve b))))
+        (else (+ a (* a (- b 1))))))
+
