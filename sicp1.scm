ddbd519a36f2823fda050d1e6e4145537f627d14
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2018-12-23 09:55:00 -0800

    Exercise 1.26

--- sicp1.scm
+++ sicp1.scm
@@ -922,3 +922,31 @@ Depending on the behavior of large values of `base' and `exp' combined with
 the system's handling of large numbers, it is either a middling gain or an
 enormously slower operation. |#
 
+
+#| Exercise 1.26
+Louis Reasoner is having great difficulty doing *Note Exercise 1.24. His
+`fast-prime?' test seems to run more slowly than his `prime?' test. Louis
+calls his friend Eva Lu Ator over to help. When they examine Louis's code,
+they find that he has rewritten the `expmod' procedure to use an explicit
+multiplication, rather than calling `square':
+
+          (define (expmod base exp m)
+            (cond ((= exp 0) 1)
+                  ((even? exp)
+                   (remainder (* (expmod base (/ exp 2) m)
+                                 (expmod base (/ exp 2) m))
+                              m))
+                  (else
+                   (remainder (* base (expmod base (- exp 1) m))
+                              m))))
+
+"I don't see what difference that could make," says Louis. "I do."
+says Eva. "By writing the procedure like that, you have transformed the
+[theta](`log' n) process into a [theta](n) process." Explain. |#
+
+#| Answer:
+Assuming the computer doesn't perform any sort of sophisticated
+memoization, effectively each step is performing twice as much work for n
+steps, e.g n^2, trimming the speed of the original implementation down to
+[theta](n). |#
+
