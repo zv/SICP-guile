cd5f8bf8da6b72199bd0cd25571b89834bec3850
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2018-12-22 18:05:34 -0800

    Exercise 1.23

--- sicp1.scm
+++ sicp1.scm
@@ -875,3 +875,23 @@ proportional to the number of steps required for the computation?
   (driver start '() 0))
 ;;; TODO XXX write test
 
+
+#| Exercise 1.23
+The `smallest-divisor' procedure shown at the start of this section does
+lots of needless testing: After it checks to see if the number is divisible
+by 2 there is no point in checking to see if it is divisible by any larger
+even numbers. This suggests that the values used for `test-divisor' should
+not be 2, 3, 4, 5, 6, ..., but rather 2, 3, 5, 7, 9, .... To implement this
+change, define a procedure `next' that returns 3 if its input is equal to 2
+and otherwise returns its input plus 2. Modify the `smallest-divisor'
+procedure to use `(next test-divisor)' instead of `(+ test-divisor 1)'.
+With `timed-prime-test' incorporating this modified version of
+`smallest-divisor', run the test for each of the 12 primes found Note in 1.22
+Since this modification halves the number of test steps, you should expect
+it to run about twice as fast. Is this expectation confirmed? If not, what
+is the observed ratio of the speeds of the two algorithms, and how do you
+explain the fact that it is different from 2?
+|#
+
+(define (1.23/next n) (if (= n 2) 3 (+ n 2)))
+
