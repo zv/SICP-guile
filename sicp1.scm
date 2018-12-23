7b2a5034c171a73d100622d6e21d83511681c417
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2018-12-22 16:55:34 -0800

    Exercise 1.20

--- sicp1.scm
+++ sicp1.scm
@@ -771,3 +771,20 @@ logarithmic number of steps:
                         (- count 1)))))
 ;; TODO XXX write test
 
+
+#| Exercise 1.20
+The process that a procedure generates is of course dependent on the rules
+used by the interpreter. As an example, consider the iterative `gcd'
+procedure given above. Suppose we were to interpret this procedure using
+normal-order evaluation, as discussed in section *Note 1-1-5. (The
+normal-order-evaluation rule for `if' is described in *Note Exercise 1-5)
+Using the substitution method (for normal order), illustrate the process
+generated in evaluating `(gcd 206 40)' and indicate the `remainder'
+operations that are actually performed. How many `remainder' operations are
+actually performed in the normal-order evaluation of `(gcd 206 40)'? In the
+applicative-order evaluation?
+|#
+
+#| Answer
+Performs 18 `remainder' operations
+|#
