5ecd9ce486c82dd944457d44ab6b5d2964452519
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2018-12-28 00:18:23 -0800

    Exercise 1.16

--- sicp1.scm
+++ sicp1.scm
@@ -672,3 +672,25 @@ a. The procedure is evaluated 5 times
 b. The order of growth is O(log(n))
 |#
 
+
+#| Exercise 1.16
+Design a procedure that evolves an iterative exponentiation process that
+uses successive squaring and uses a logarithmic number of steps, as does
+`fast-expt'.
+
+(Hint: Using the observation that (bⁿ/²)²= (b²)ⁿ/², keep, along with the
+exponent `n' and the base `b', an additional state variable `a', and define
+the state transformation in such a way that the product abⁿ is unchanged
+from state to state. At the beginning of the process a is taken to be 1,
+and the answer is given by the value of `a' at the end of the process. In
+general, the technique of defining an "invariant quantity" that remains
+unchanged from state to state is a powerful way to think about the design
+of iterative algorithms.)
+|#
+
+(define (zv/expt-iter b n a)
+  (cond
+   [(= n 0) a]
+   [(even? n) (zv/expt-iter (* b b) (/ n 2) a)]
+   [else      (zv/expt-iter  b (- n 1) (* a b))]))
+
