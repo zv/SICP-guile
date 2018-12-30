000899ac2718a618c48da912a7bf345131e75678
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2018-12-29 17:44:28 -0800

    Exercise 1.30

--- sicp1.scm
+++ sicp1.scm
@@ -694,3 +694,16 @@ the results to those of the integral procedure shown above. |#
     (* (/ h 3)
        (+ (f a) (sum fy 0 inc n) (f (+ a (* n h)))))))
 
+
+#| Exercise 1.30
+The sum procedure above generates a linear recursion. The procedure can be
+rewritten so that the sum is performed iteratively. |#
+
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ (term a) result))))
+
+  (iter a 0))
+
