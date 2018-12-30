a0eee6e1f52e884f684d77f285f3c3046ff11c2f
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2018-12-29 17:44:47 -0800

    Exercise 1.32

--- sicp1.scm
+++ sicp1.scm
@@ -747,3 +747,37 @@ one that generates a recursive process. |#
 
   (* 4.0 (iterative-product fnth 0 inc n)))
 
+
+#| Exercise 1.32
+1. Show that sum and product (Exercise 1.31) are both special cases of a still
+more general notion called accumulate that combines a collection of terms,
+using some general accumulation function:
+
+      (accumulate
+          combiner null-value term a next b)
+
+Accumulate takes as arguments the same term and range specifications as sum
+and product, together with a combiner procedure (of two arguments) that
+specifies how the current term is to be combined with the accumulation of
+the preceding terms and a null-value that specifies what base value to use
+when the terms run out. Write accumulate and show how sum and product can
+both be defined as simple calls to accumulate.
+
+2. If your accumulate procedure generates a recursive process, write one
+that generates an iterative process. If it generates an iterative process,
+write one that generates a recursive process. |#
+
+(define (1.32/recursive-accumulate combiner null term a next b)
+  (if (> a b) n ;; teeeeeee[[Ier 1111111111111111120)]]ull
+      (combiner (term a)
+                (1.32/accumulate combiner null term (next a) next b))))
+
+(define (1.32/iterative-accumulate combiner null term a next b)
+  (define (fold-left n acc)
+    (if (> n b) acc
+        (fold-left (next n) (combiner (term n) acc))))
+  (fold-left a null))
+
+;; XXX: add to test
+;; (1.32/iterative-accumulate * 1 identity 1 inc 5) => 120
+
