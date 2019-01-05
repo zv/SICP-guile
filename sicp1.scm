eea318a795572f61cef952cac6d11164e282e587
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2019-01-05 00:21:33 -0800

    Exercise 1.42

--- sicp1.scm
+++ sicp1.scm
@@ -1351,3 +1351,17 @@ For example, if inc is a procedure that adds 1 to its argument, then
 
 (define (1.41/double fn) (λ (n) (fn (fn n))))
 
+
+#| Exercise 1.42
+Let f and g be two one-argument functions. The composition f after g is
+defined to be the function x↦f(g(x)). Define a procedure compose that
+implements composition. For example, if inc is a procedure that adds 1 to
+its argument,
+
+    ((compose square inc) 6)
+    49
+
+|#
+
+(define (1.42/compose f g) (λ (n) (f (g n))))
+
