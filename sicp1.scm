2489b4b0eb9b29de777d34e6cc8440bd8393f4d3
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2019-01-05 00:20:27 -0800

    Exercise 1.36

--- sicp1.scm
+++ sicp1.scm
@@ -1170,3 +1170,14 @@ procedure. |#
 (define (1.35/find-golden-ratio)
   (fixed-point (λ (n) (+ 1 (/ 1 n))) 1))
 
+
+#| Exercise 1.36
+Modify fixed-point so that it prints the sequence of approximations it
+generates, using the newline and display primitives shown in Exercise 1.22.
+Then find a solution to xⁿ=1000 by finding a fixed point of
+x↦log(1000)/log(x). (Use Scheme’s primitive log procedure, which computes
+natural logarithms.) Compare the number of steps this takes with and
+without average damping. (Note that you cannot start fixed-point with a
+guess of 1, as this would cause division by log(1)=0.)
+|#
+
