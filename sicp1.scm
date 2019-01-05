bf9460b9f7381b54163094e559396ff4714d4a3b
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2019-01-05 00:21:27 -0800

    Exercise 1.41

--- sicp1.scm
+++ sicp1.scm
@@ -1338,3 +1338,16 @@ to approximate zeros of the cubic x³+ax²+bx+c. |#
        (* b x)
        c)))
 
+
+#| Exercise 1.41
+Define a procedure double that takes a procedure of one argument as
+argument and returns a procedure that applies the original procedure twice.
+For example, if inc is a procedure that adds 1 to its argument, then
+(double inc) should be a procedure that adds 2. What value is returned by
+
+    (((double (double double)) inc) 5)
+
+? |#
+
+(define (1.41/double fn) (λ (n) (fn (fn n))))
+
