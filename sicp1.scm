1389557d088168fa3959fac3a9fd1748d8b6da36
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2019-01-05 00:21:06 -0800

    Exercise 1.38

--- sicp1.scm
+++ sicp1.scm
@@ -1281,3 +1281,20 @@ b. If your `cont-frac' procedure generates a recursive process,
   (nth-continuation 1 0))
 
 (define cont-frac 1.37/cont-frac-iter)
+
+#| Exercise 1.38
+In 1737, the Swiss mathematician Leonhard Euler published a memoir `De
+Fractionibus Continuis', which included a continued fraction expansion for
+e - 2, where e is the base of the natural logarithms. In this fraction, the
+nᵢ are all 1, and the Dᵢ are successively 1, 2, 1, 1, 4, 1, 1, 6, 1, 1,
+8, .... Write a program that uses your `cont-frac' procedure from Exercise
+1.37 to approximate e, based on Euler's expansion. |#
+
+(define (e-2 k)
+  (cont-frac
+   (λ (i) 1.0)
+   (λ (n) (if (= 0 (modulo (+ n 1) 3))
+              (* 2 (/ (+ n 1) 3))
+              1))
+   k))
+
