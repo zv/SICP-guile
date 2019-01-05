a482936bfa4e4cc9cb155bbdc47b2dee096e0813
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2019-01-05 00:21:43 -0800

    Exercise 1.43

--- sicp1.scm
+++ sicp1.scm
@@ -1365,3 +1365,26 @@ its argument,
 
 (define (1.42/compose f g) (λ (n) (f (g n))))
 
+
+#| Exercise 1.43
+If f is a numerical function and n is a positive integer, then we can form
+the nth repeated application of f, which is defined to be the function
+whose value at x is f(f(…(f(x))…)). For example, if f is the function
+x↦x+1, then the nth repeated application of f is the function x↦x+n. If f
+is the operation of squaring a number, then the nth repeated application of
+f is the function that raises its argument to the 2n-th power. Write a
+procedure that takes as inputs a procedure that computes f and a positive
+integer n and returns the procedure that computes the nth repeated
+application of f. Your procedure should be able to be used as follows:
+
+    ((repeated square 2) 5)
+    625
+
+Hint: You may find it convenient to use compose from Exercise 1.42. |#
+
+(define (1.43/repeated-apply fn times)
+  (if (= times 1) (λ (n) (fn n))
+      (λ (n)
+        (fn
+         ((1.43/repeated-apply fn (- times 1)) n)))))
+
