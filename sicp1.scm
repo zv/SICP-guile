e32101b28890f64b8da2e1aa800aa40539bdd69b
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2019-01-05 00:21:11 -0800

    Exercise 1.39

--- sicp1.scm
+++ sicp1.scm
@@ -1298,3 +1298,27 @@ nᵢ are all 1, and the Dᵢ are successively 1, 2, 1, 1, 4, 1, 1, 6, 1, 1,
               1))
    k))
 
+
+#| Exercise 1.39
+A continued fraction representation of the
+tangent function was published in 1770 by the German mathematician
+J.H. Lambert:
+
+x
+tan x = ---------------
+x^2
+1 - -----------
+x^2
+3 - -------
+5  - ...
+
+where x is in radians.  Define a procedure `(tan-cf x k)' that
+computes an approximation to the tangent function based on
+Lambert's formula.  `K' specifies the number of terms to compute,
+as in *Note Exercise 1.37 |#
+
+(define (1.39/tan-cf x k)
+  (cont-frac (λ (i) (if (= i 1) x (* -1.0 (* x x))))
+             (λ (i) (- (* i 2) 1.0))
+             k))
+
