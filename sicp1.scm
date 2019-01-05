f556f7681539ba7cbe5caa66bf01b668777c64e9
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2019-01-05 00:21:50 -0800

    Exercise 1.44

--- sicp1.scm
+++ sicp1.scm
@@ -1388,3 +1388,22 @@ Hint: You may find it convenient to use compose from Exercise 1.42. |#
         (fn
          ((1.43/repeated-apply fn (- times 1)) n)))))
 
+
+#| Exercise 1.44
+The idea of smoothing a function is an important concept in signal
+processing. If f is a function and dx is some small number, then the
+smoothed version of f is the function whose value at a point x is the
+average of f(x−dx), f(x), and f(x+dx). Write a procedure smooth that takes
+as input a procedure that computes f and returns a procedure that computes
+the smoothed f. It is sometimes valuable to repeatedly smooth a function
+(that is, smooth the smoothed function, and so on) to obtain the n-fold
+smoothed function. Show how to generate the n-fold smoothed function of any
+given function using smooth and repeated from Exercise 1.43. |#
+
+(define (1.44/smooth f)
+  (λ (x)
+    (/ (+ (f (- x dx))
+          (f x)
+          (f (+ x dx)))
+       3)))
+
