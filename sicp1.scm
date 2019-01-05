792ab15a9183939d726793382773384df655223a
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2019-01-05 00:22:05 -0800

    Exercise 1.46

--- sicp1.scm
+++ sicp1.scm
@@ -1423,3 +1423,36 @@ simple procedure for computing nth roots using fixed-point, average-damp,
 and the repeated procedure of Exercise 1.43. Assume that any arithmetic
 operations you need are available as primitives. |#
 
+
+#| Exercise 1.46
+Several of the numerical methods described in this chapter are instances of
+an extremely general computational strategy known as iterative improvement.
+Iterative improvement says that, to compute something, we start with an
+initial guess for the answer, test if the guess is good enough, and
+otherwise improve the guess and continue the process using the improved
+guess as the new guess. Write a procedure iterative-improve that takes two
+procedures as arguments: a method for telling whether a guess is good
+enough and a method for improving a guess. Iterative-improve should return
+as its value a procedure that takes a guess as argument and keeps improving
+the guess until it is good enough. Rewrite the sqrt procedure of 1.1.7 and
+the fixed-point procedure of 1.3.3 in terms of iterative-improve. |#
+
+(define (iterative-improve good-enough? improve)
+  (λ (guess)
+    (let ([improved (improve guess)])
+      (if (good-enough? guess improved) guess
+          ((iterative-improve good-enough? improve) improved)))))
+
+(define (1.46/iterative-sqrt n)
+  (iterative-improve
+   (λ (guess improved) (< (abs (- guess improved)) 0.001))
+   (λ (guess)
+     (average guess (/ n guess)))))
+
+(define (1.46/fixed-point f first-guess)
+  ((iterative-improve
+    (λ (guess)
+      (< (abs (- (f guess) guess))
+         0.00001))
+    (λ (guess) (f guess)))
+   first-guess))
