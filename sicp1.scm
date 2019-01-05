860128615031bb68597027982184c5b507c07bae
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2019-01-05 00:20:17 -0800

    Exercise 1.35

--- sicp1.scm
+++ sicp1.scm
@@ -1161,3 +1161,12 @@ Explain. |#
 
 Infinite Loop |#
 
+
+#| Exercise 1.35
+Show that the golden ratio φ (1.2.2) is a fixed point of the transformation
+x↦1+1/x, and use this fact to compute φ by means of the fixed-point
+procedure. |#
+
+(define (1.35/find-golden-ratio)
+  (fixed-point (λ (n) (+ 1 (/ 1 n))) 1))
+
